c45d2b2bd77bd1b074407640fee12e9a2ebc12a0
tomoohive <graphelate@gmail.com>
2021-11-10 15:39:29 +0900

    refactoring

---
+++ split_utils.py
@@ -0,0 +1,44 @@
+import glob
+import os
+
+import cv2
+from random_name import random_name
+
+
+def _save_images(crops: list):
+    for c in crops:
+        cv2.imwrite(output_dir+'/moss_'+ random_name(10) +'.png', c)
+
+
+def _crop_texture(image_files: list, height: int, width: int):
+    crops = []
+    for i_f in image_files:
+        image = cv2.imread(i_f)
+        im_height, im_width, _ = image.shape[:3]
+        split_height, split_width = im_height//height, im_width//width
+
+        for s_h in range(split_height):
+            for s_w in range(split_width):
+                w = s_w * height
+                h = s_h * width
+                c = image[h:h+height, w:w+width, :]
+                crops.append(c)
+
+    return crops
+
+
+def split_texture(input_dir: str, output_dir: str, height: int, width: int):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    image_files = glob.glob(os.path.join(input_dir, '*'))
+    crops = _crop_texture(image_files=image_files, height=height, width=width)
+    _save_images(crops=crops)
+    
+
+if __name__ == '__main__':
+    input_dir = './moss_textures'
+    output_dir = './result'
+    height, width = 256, 256
+
+    split_texture(input_dir=input_dir, output_dir=output_dir, height=height, width=width)
